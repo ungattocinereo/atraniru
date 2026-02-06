ba6dfa2304ec4407218aa2c2028b74ae0af3d5e8
root <root@cristallpont.local>
2026-02-06 18:03:07 +0100

    static precomressed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    compress({
+      brotli: true,
+      gzip: true,
+      html: true,
+      css: true,
+      js: true,
+      svg: true,
+      img: false, 
+      image: false 
+    }),
+  ],
+});
